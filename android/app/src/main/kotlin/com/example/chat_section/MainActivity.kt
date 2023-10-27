f2b0454697bcdb9f33370e65bac8b8a7fc9cc04f
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-10-27 17:48:57 +0200

    git init

---
+++ android/app/src/main/kotlin/com/example/chat_section/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_section
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:chat_section/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/welcome_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MessageMe app',
+
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RegistrationScreen(),
+    );
+  }
+}
